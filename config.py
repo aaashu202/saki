8e5a1b6750c6b3aa188bac46ec978f3a599873e5
aaashu202 <167896518+aaashu202@users.noreply.github.com>
2026-07-17 21:58:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,8 +40,8 @@ START_PIC = os.environ.get("START_PIC", "https://i.ibb.co/LDMcWdYT/file-923.jpg"
 FORCE_PIC = os.environ.get("FORCE_PIC", "https://iili.io/3bQtwWN.md.jpg")
 
 #--------------------------------------------
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "linkshortify.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "105c56928e54e112d88930006173fcd0e1d1ec73")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "urlpx.in")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "bba4d829688ece581f832b499bcfcb3d8be24dd7")
 TUT_VID = os.environ.get("TUT_VID","https://t.me/korean_dramas_bot_Dramafilez/9")
 SHORT_MSG = "<b>⌯ Here is Your Download Link, Must Watch Tutorial Before Clicking On Download...</b>"
 
